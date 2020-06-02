b19beb2ccc2260282515041b18be227ad0ddc8d9
DelleVelleD <delle.caj@gmail.com>
2020-06-02 15:59:18 -0400

    Add mainframe class and XML; Reorganize

---
+++ Source/Building_NeurolinkMainframe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Neurolink {
+	public abstract class Building_NeurolinkMainframe : Building, IThingHolder {
+		public ThingOwner innerContainer = null;
+
+		public Building_NeurolinkMainframe() {
+			this.innerContainer = new ThingOwner<Thing>(this, false, LookMode.Deep);
+		}
+
+
+
+
+
+
+		public void GetChildHolders(List<IThingHolder> outChildren) {
+			throw new System.NotImplementedException();
+		}
+
+		public ThingOwner GetDirectlyHeldThings() {
+			throw new System.NotImplementedException();
+		}
+	}
+}
