5f608da82f3a4a99b2e83a6c49f3011a4e4e4c0a
DelleVelleD <delle.caj@gmail.com>
2020-06-04 04:47:21 -0400

    Add mainframe structure; Add mainframe recipes structure; Add ThingCategories

--- Source/Building_NeurolinkMainframe.cs
+++ Source/Building_NeurolinkMainframe.cs
@@ -1,25 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using RimWorld;
 using Verse;
 
 namespace Neurolink {
-	public abstract class Building_NeurolinkMainframe : Building, IThingHolder {
-		public ThingOwner innerContainer = null;
+	public class Building_NeurolinkMainframe : Building_WorkTable {
 
-		public Building_NeurolinkMainframe() {
-			this.innerContainer = new ThingOwner<Thing>(this, false, LookMode.Deep);
-		}
+		
 
+		
 
 
 
-
-
-		public void GetChildHolders(List<IThingHolder> outChildren) {
-			throw new System.NotImplementedException();
-		}
-
-		public ThingOwner GetDirectlyHeldThings() {
-			throw new System.NotImplementedException();
-		}
 	}
 }
