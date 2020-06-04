c60b0bc9fdb734aa853fe90e44371cdb0d77dfb3
DelleVelleD <delle.caj@gmail.com>
2020-06-04 19:25:31 -0400

    Redo mainframe structure; Remove mainframe bills

--- Source/Building_NeurolinkMainframe.cs
+++ Source/Building_NeurolinkMainframe.cs
@@ -1,14 +1,22 @@
-﻿using RimWorld;
+﻿using System.Collections.Generic;
+using RimWorld;
 using Verse;
 
 namespace Neurolink {
-	public class Building_NeurolinkMainframe : Building_WorkTable {
+	public class Building_NeurolinkMainframe : Building, IThingHolder {
 
-		
-
-		
+		protected ThingOwner innerContainer = null;
 
 
 
+
+
+		public void GetChildHolders(List<IThingHolder> outChildren) {
+			ThingOwnerUtility.AppendThingHoldersFromThings(outChildren, GetDirectlyHeldThings());
+		}
+
+		public ThingOwner GetDirectlyHeldThings() {
+			return innerContainer;
+		}
 	}
 }
